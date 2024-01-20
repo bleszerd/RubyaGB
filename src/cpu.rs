08807f6f62603a5136dec4917848857912b175bb
Vinicius Resende <bleszerd@gmail.com>
2024-01-20 11:14:28 -0300

    feat: Initializing cartridge header

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use std::{fs, os::unix::fs::MetadataExt};
+
 /**
  * The file is named as "cpu" but are a Singleton,
  * should be splitted latter.
@@ -10,6 +12,102 @@ const CARRY_FLAG_BYTE_POSITION: u8 = 4;
 
 const SANITIZE_MOST_SIGNIFICANT_BYTE: u16 = 0xFF00;
 const SANITIZE_LESS_SIGNIFICANT_BYTE: u16 = 0xFF;
+
+/** === Licensee code === */
+const LICENSEE_NONE: u8 = 0x00;
+const LICENSEE_NINTENDO_RD1: u8 = 0x01;
+const LICENSEE_CAPCOM: u8 = 0x08;
+const LICENSEE_ELETRONIC_ARTS: u8 = 0x13;
+const LICENSEE_HUDSON_SOFT: u8 = 0x18;
+const LICENSEE_B_AI: u8 = 0x19;
+const LICENSEE_KSS: u8 = 0x20;
+const LICENSEE_POW: u8 = 0x22;
+const LICENSEE_PCM_COMPLETE: u8 = 0x24;
+const LICENSEE_SAN_X: u8 = 0x25;
+const LICENSEE_KEMCO_JAPAN: u8 = 0x28;
+const LICENSEE_SETA: u8 = 0x29;
+const LICENSEE_VIACOM: u8 = 0x30;
+const LICENSEE_NINTENDO: u8 = 0x31;
+const LICENSEE_BANDAI: u8 = 0x32;
+const LICENSEE_OCEAN_ACCLAIM: u8 = 0x33;
+const LICENSEE_KONAMI: u8 = 0x34;
+const LICENSEE_HECTOR: u8 = 0x35;
+const LICENSEE_TAITO: u8 = 0x37;
+const LICENSEE_HUDSON: u8 = 0x38;
+const LICENSEE_BANPRESTO: u8 = 0x39;
+const LICENSEE_UBISOFT: u8 = 0x41;
+const LICENSEE_ATLUS: u8 = 0x42;
+const LICENSEE_MALIBU: u8 = 0x44;
+const LICENSEE_ANGEL: u8 = 0x46;
+const LICENSEE_BULLET_PROOF: u8 = 0x47;
+const LICENSEE_IREM: u8 = 0x49;
+const LICENSEE_ABSOLUTE: u8 = 0x50;
+const LICENSEE_ACCLAIM: u8 = 0x51;
+const LICENSEE_ACTIVISION: u8 = 0x52;
+const LICENSEE_AMERICAN_SAMMY: u8 = 0x53;
+const LICENSEE_KONAMI_2: u8 = 0x54;
+const LICENSEE_HITECH_ENTERTAINMENT: u8 = 0x55;
+const LICENSEE_LJN: u8 = 0x56;
+const LICENSEE_MATCHBOX: u8 = 0x57;
+const LICENSEE_MATTEL: u8 = 0x58;
+const LICENSEE_MILTON_BRADLEY: u8 = 0x59;
+const LICENSEE_TITUS: u8 = 0x60;
+const LICENSEE_VIRGIN: u8 = 0x61;
+const LICENSEE_LUCAS_ARTS: u8 = 0x64;
+const LICENSEE_OCEAN: u8 = 0x67;
+const LICENSEE_ELETRONIC_ARTS_2: u8 = 0x69;
+const LICENSEE_INFOGRAMES: u8 = 0x70;
+const LICENSEE_INTERPLAY: u8 = 0x71;
+const LICENSEE_BRODERBUND: u8 = 0x72;
+const LICENSEE_SCULPTURED: u8 = 0x73;
+const LICENSEE_SCI: u8 = 0x75;
+const LICENSEE_THQ: u8 = 0x78;
+const LICENSEE_ACCOLADE: u8 = 0x79;
+const LICENSEE_MISAWA: u8 = 0x80;
+const LICENSEE_LOZC: u8 = 0x83;
+const LICENSEE_TOKUMA_SHOTEN_INTERMEDIA: u8 = 0x86;
+const LICENSEE_TSUKUDA_ORIGINAL: u8 = 0x87;
+const LICENSEE_CHUNSOFT: u8 = 0x91;
+const LICENSEE_VIDEO_SYSTEM: u8 = 0x92;
+const LICENSEE_OCEAN_ACCLAIM_2: u8 = 0x93;
+const LICENSEE_VARIE: u8 = 0x95;
+const LICENSEE_YONEZAWA_SPAL: u8 = 0x96;
+const LICENSEE_KANEKO: u8 = 0x97;
+const LICENSEE_PACK_IN_SOFT: u8 = 0x99;
+const LICENSEE_BOTTOM_UP: u8 = 0x9A;
+const LICENSEE_KONAMI_YU_GI_OH: u8 = 0xA4;
+/** === Licenssee code === */
+
+/** === Cartridge type === */
+const CARTRIDGE_TYPE_ROM_ONLY: u8 = 0x00;
+const CARTRIDGE_TYPE_MBC1: u8 = 0x01;
+const CARTRIDGE_TYPE_MBC1_RAM: u8 = 0x02;
+const CARTRIDGE_TYPE_MBC1_RAM_BATTERY: u8 = 0x03;
+const CARTRIDGE_TYPE_MBC2: u8 = 0x05;
+const CARTRIDGE_TYPE_MBC2_BATTERY: u8 = 0x06;
+const CARTRIDGE_TYPE_ROM_RAM1: u8 = 0x08;
+const CARTRIDGE_TYPE_ROM_RAM1_BATTERY: u8 = 0x09;
+const CARTRIDGE_TYPE_MMM01: u8 = 0x0B;
+const CARTRIDGE_TYPE_MMM01_RAM: u8 = 0x0C;
+const CARTRIDGE_TYPE_MMM01_RAM_BATTERY: u8 = 0x0D;
+const CARTRIDGE_TYPE_MBC3_TIMER_BATTERY: u8 = 0x0F;
+const CARTRIDGE_TYPE_MBC3_TIMER_RAM_BATTERY2: u8 = 0x10;
+const CARTRIDGE_TYPE_MBC3: u8 = 0x11;
+const CARTRIDGE_TYPE_MBC3_RAM2: u8 = 0x12;
+const CARTRIDGE_TYPE_MBC3_RAM_BATTERY2: u8 = 0x13;
+const CARTRIDGE_TYPE_MBC5: u8 = 0x19;
+const CARTRIDGE_TYPE_MBC5_RAM: u8 = 0x1A;
+const CARTRIDGE_TYPE_MBC5_RAM_BATTERY: u8 = 0x1B;
+const CARTRIDGE_TYPE_MBC5_RUMBLE: u8 = 0x1C;
+const CARTRIDGE_TYPE_MBC5_RUMBLE_RAM: u8 = 0x1D;
+const CARTRIDGE_TYPE_MBC5_RUMBLE_RAM_BATTERY: u8 = 0x1E;
+const CARTRIDGE_TYPE_MBC6: u8 = 0x20;
+const CARTRIDGE_TYPE_MBC7_SENSOR_RUMBLE_RAM_BATTERY: u8 = 0x22;
+const CARTRIDGE_TYPE_POCKET_CAMERA: u8 = 0xFC;
+const CARTRIDGE_TYPE_BANDAI_TAMA5: u8 = 0xFD;
+const CARTRIDGE_TYPE_HU_C3: u8 = 0xFE;
+const CARTRIDGE_TYPE_HU_C1_RAM_BATTERY: u8 = 0xFF;
+/** === Cartridge type === */
 
 #[derive(Debug)]
 enum Instruction {
@@ -123,32 +221,192 @@ enum LoadType {
     Byte(LoadByteTarget, LoadByteSource),
 }
 
-struct Registers {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    f: FlagsRegister,
-    h: u8,
-    l: u8,
+pub struct Registers {
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: FlagsRegister,
+    pub h: u8,
+    pub l: u8,
 }
 
-struct FlagsRegister {
-    zero: bool,
-    subtract: bool,
-    half_carry: bool,
-    carry: bool,
+pub struct FlagsRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool,
 }
 
-struct CPU {
-    registers: Registers,
-    pc: u16,
-    bus: MemoryBus,
+pub struct CPU {
+    pub registers: Registers,
+    pub pc: u16,
+    pub bus: MemoryBus,
 }
 
-struct MemoryBus {
-    memory: [u8; 0xFFFF],
+pub struct MemoryBus {
+    pub memory: [u8; 0xFFFF],
+}
+
+#[derive(Debug)]
+
+pub struct CartridgeHeader {
+    // pub entry: [u8; 0x4],
+    pub logo: Vec<u8>,
+    pub title: String,
+    // pub new_lic_code: u16,
+    pub lic_code: u8,
+    // pub sbg_flag: u8,
+    pub r#type: u8,
+    pub cartridge_size: u8,
+    // pub ram_size: u8,
+    // pub dest_code: u8,
+    pub version: u8,
+    // pub checksum: u8,
+    // pub global_checksum: u16,
+}
+
+struct CartridgeContext {
+    filename: [char; 1024],
+    cartridge_size: u32,
+    cartridge_data: Vec<u8>,
+    cartridge_header: &'static CartridgeHeader,
+}
+
+impl CartridgeHeader {
+    pub fn licensee_bytes_from_cartridge_header(&self, bytes: &[u8]) -> u8 {
+        let byte_value = bytes[0] ^ bytes[1];
+
+        bytes[0] ^ bytes[1]
+    }
+
+    fn licensee_brand_from_cartridge_header(&self, bytes: &[u8]) -> String {
+        let byte_value = self.licensee_bytes_from_cartridge_header(bytes);
+
+        match byte_value {
+            LICENSEE_NONE => String::from("NONE"),
+            LICENSEE_NINTENDO_RD1 => String::from("NINTENDO_RD1"),
+            LICENSEE_CAPCOM => String::from("CAPCOM"),
+            LICENSEE_ELETRONIC_ARTS => String::from("ELETRONIC_ARTS"),
+            LICENSEE_HUDSON_SOFT => String::from("HUDSON_SOFT"),
+            LICENSEE_B_AI => String::from("B_AI"),
+            LICENSEE_KSS => String::from("KSS"),
+            LICENSEE_POW => String::from("POW"),
+            LICENSEE_PCM_COMPLETE => String::from("PCM_COMPLETE"),
+            LICENSEE_SAN_X => String::from("SAN_X"),
+            LICENSEE_KEMCO_JAPAN => String::from("KEMCO_JAPAN"),
+            LICENSEE_SETA => String::from("SETA"),
+            LICENSEE_VIACOM => String::from("VIACOM"),
+            LICENSEE_NINTENDO => String::from("NINTENDO"),
+            LICENSEE_BANDAI => String::from("BANDAI"),
+            LICENSEE_OCEAN_ACCLAIM => String::from("OCEAN_ACCLAIM"),
+            LICENSEE_KONAMI => String::from("KONAMI"),
+            LICENSEE_HECTOR => String::from("HECTOR"),
+            LICENSEE_TAITO => String::from("TAITO"),
+            LICENSEE_HUDSON => String::from("HUDSON"),
+            LICENSEE_BANPRESTO => String::from("BANPRESTO"),
+            LICENSEE_UBISOFT => String::from("UBISOFT"),
+            LICENSEE_ATLUS => String::from("ATLUS"),
+            LICENSEE_MALIBU => String::from("MALIBU"),
+            LICENSEE_ANGEL => String::from("ANGEL"),
+            LICENSEE_BULLET_PROOF => String::from("BULLET_PROOF"),
+            LICENSEE_IREM => String::from("IREM"),
+            LICENSEE_ABSOLUTE => String::from("ABSOLUTE"),
+            LICENSEE_ACCLAIM => String::from("ACCLAIM"),
+            LICENSEE_ACTIVISION => String::from("ACTIVISION"),
+            LICENSEE_AMERICAN_SAMMY => String::from("AMERICAN_SAMMY"),
+            LICENSEE_KONAMI_2 => String::from("KONAMI_2"),
+            LICENSEE_HITECH_ENTERTAINMENT => String::from("HITECH_ENTERTAINMENT"),
+            LICENSEE_LJN => String::from("LJN"),
+            LICENSEE_MATCHBOX => String::from("MATCHBOX"),
+            LICENSEE_MATTEL => String::from("MATTEL"),
+            LICENSEE_MILTON_BRADLEY => String::from("MILTON_BRADLEY"),
+            LICENSEE_TITUS => String::from("TITUS"),
+            LICENSEE_VIRGIN => String::from("VIRGIN"),
+            LICENSEE_LUCAS_ARTS => String::from("LUCAS_ARTS"),
+            LICENSEE_OCEAN => String::from("OCEAN"),
+            LICENSEE_ELETRONIC_ARTS_2 => String::from("ELETRONIC_ARTS_2"),
+            LICENSEE_INFOGRAMES => String::from("INFOGRAMES"),
+            LICENSEE_INTERPLAY => String::from("INTERPLAY"),
+            LICENSEE_BRODERBUND => String::from("BRODERBUND"),
+            LICENSEE_SCULPTURED => String::from("SCULPTURED"),
+            LICENSEE_SCI => String::from("SCI"),
+            LICENSEE_THQ => String::from("THQ"),
+            LICENSEE_ACCOLADE => String::from("ACCOLADE"),
+            LICENSEE_MISAWA => String::from("MISAWA"),
+            LICENSEE_LOZC => String::from("LOZC"),
+            LICENSEE_TOKUMA_SHOTEN_INTERMEDIA => String::from("TOKUMA_SHOTEN_INTERMEDIA"),
+            LICENSEE_TSUKUDA_ORIGINAL => String::from("TSUKUDA_ORIGINAL"),
+            LICENSEE_CHUNSOFT => String::from("CHUNSOFT"),
+            LICENSEE_VIDEO_SYSTEM => String::from("VIDEO_SYSTEM"),
+            LICENSEE_OCEAN_ACCLAIM_2 => String::from("OCEAN_ACCLAIM_2"),
+            LICENSEE_VARIE => String::from("VARIE"),
+            LICENSEE_YONEZAWA_SPAL => String::from("YONEZAWA_SPAL"),
+            LICENSEE_KANEKO => String::from("KANEKO"),
+            LICENSEE_PACK_IN_SOFT => String::from("PACK_IN_SOFT"),
+            LICENSEE_BOTTOM_UP => String::from("BOTTOM_UP"),
+            LICENSEE_KONAMI_YU_GI_OH => String::from("KONAMI_YU_GI_OH"),
+            _ => panic!("No license code found!"),
+        }
+    }
+
+    fn get_cartridge_type(type_code: u8) -> String {
+        match type_code {
+            CARTRIDGE_TYPE_ROM_ONLY => String::from("ROM_ONLY"),
+            CARTRIDGE_TYPE_MBC1 => String::from("MBC1"),
+            CARTRIDGE_TYPE_MBC1_RAM => String::from("MBC1_RAM"),
+            CARTRIDGE_TYPE_MBC1_RAM_BATTERY => String::from("MBC1_RAM_BATTERY"),
+            CARTRIDGE_TYPE_MBC2 => String::from("MBC2"),
+            CARTRIDGE_TYPE_MBC2_BATTERY => String::from("MBC2_BATTERY"),
+            CARTRIDGE_TYPE_ROM_RAM1 => String::from("ROM_RAM1"),
+            CARTRIDGE_TYPE_ROM_RAM1_BATTERY => String::from("ROM_RAM1_BATTERY"),
+            CARTRIDGE_TYPE_MMM01 => String::from("MMM01"),
+            CARTRIDGE_TYPE_MMM01_RAM => String::from("MMM01_RAM"),
+            CARTRIDGE_TYPE_MMM01_RAM_BATTERY => String::from("MMM01_RAM_BATTERY"),
+            CARTRIDGE_TYPE_MBC3_TIMER_BATTERY => String::from("MBC3_TIMER_BATTERY"),
+            CARTRIDGE_TYPE_MBC3_TIMER_RAM_BATTERY2 => String::from("MBC3_TIMER_RAM_BATTERY2"),
+            CARTRIDGE_TYPE_MBC3 => String::from("MBC3"),
+            CARTRIDGE_TYPE_MBC3_RAM2 => String::from("MBC3_RAM2"),
+            CARTRIDGE_TYPE_MBC3_RAM_BATTERY2 => String::from("MBC3_RAM_BATTERY2"),
+            CARTRIDGE_TYPE_MBC5 => String::from("MBC5"),
+            CARTRIDGE_TYPE_MBC5_RAM => String::from("MBC5_RAM"),
+            CARTRIDGE_TYPE_MBC5_RAM_BATTERY => String::from("MBC5_RAM_BATTERY"),
+            CARTRIDGE_TYPE_MBC5_RUMBLE => String::from("MBC5_RUMBLE"),
+            CARTRIDGE_TYPE_MBC5_RUMBLE_RAM => String::from("MBC5_RUMBLE_RAM"),
+            CARTRIDGE_TYPE_MBC5_RUMBLE_RAM_BATTERY => String::from("MBC5_RUMBLE_RAM_BATTERY"),
+            CARTRIDGE_TYPE_MBC6 => String::from("MBC6"),
+            CARTRIDGE_TYPE_MBC7_SENSOR_RUMBLE_RAM_BATTERY => {
+                String::from("MBC7_SENSOR_RUMBLE_RAM_BATTERY")
+            }
+            CARTRIDGE_TYPE_POCKET_CAMERA => String::from("POCKET_CAMERA"),
+            CARTRIDGE_TYPE_BANDAI_TAMA5 => String::from("BANDAI_TAMA5"),
+            CARTRIDGE_TYPE_HU_C3 => String::from("HU_C3"),
+            CARTRIDGE_TYPE_HU_C1_RAM_BATTERY => String::from("HU_C1_RAM_BATTERY"),
+            _ => panic!("No cartridge type found!"),
+        }
+    }
+
+    pub fn init(rom_buffer: Vec<u8>, metadata: fs::Metadata) -> CartridgeHeader {
+        let mut cartridge_header = CartridgeHeader {
+            cartridge_size: 0,
+            lic_code: 0,
+            logo: vec![],
+            title: String::new(),
+            r#type: 0,
+            version: 0,
+        };
+
+        cartridge_header.cartridge_size = rom_buffer[0x148];
+        cartridge_header.lic_code =
+            cartridge_header.licensee_bytes_from_cartridge_header(&rom_buffer[0x144..=0x145]);
+        cartridge_header.logo = rom_buffer[0x104..=0x133].to_vec();
+        cartridge_header.title = String::from_utf8_lossy(&rom_buffer[0x134..=0x143]).to_string();
+        cartridge_header.r#type = rom_buffer[0x147];
+        cartridge_header.version = rom_buffer[0x14c];
+
+        cartridge_header
+    }
 }
 
 impl Registers {
@@ -253,6 +511,7 @@ impl CPU {
                     ArithmeticTarget::C => {
                         let current_value = self.registers.c;
                         let new_value = self.add(current_value);
+
                         self.registers.a = new_value;
                         self.pc.wrapping_add(1)
                     }
@@ -650,7 +909,7 @@ impl CPU {
         }
     }
 
-    fn step(&mut self) {
+    pub fn step(&mut self) {
         let mut instruction_byte = self.bus.read_byte(self.pc);
         let prefixed = instruction_byte == 0xCB;
 
@@ -697,6 +956,7 @@ impl Instruction {
     fn from_not_prefixed(byte: u8) -> Option<Instruction> {
         // TODO: Search differences between 0x34 / 0x23
         // TODO: Search differences between 0x2B / 0x35
+
         match byte {
             /* INC Opc */
             0x03 => Some(Instruction::INC(IncDecTarget::BC)),
@@ -725,6 +985,9 @@ impl Instruction {
             // 0x35 => Some(Instruction::DEC(IncDecTarget::HL)),
             0x3B => Some(Instruction::DEC(IncDecTarget::SP)),
             0x3D => Some(Instruction::DEC(IncDecTarget::A)),
+
+            /* ==== JUST FOR TEST - DELETE THIS ==== */
+            0x65 => Some(Instruction::ADD(ArithmeticTarget::C)),
 
             /* Fallback */
             _ => panic!("Non-prefixed 0x{:?} not implemented!", byte),
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@
 
 mod cpu;
 
+use std::{
+    fs::{self, File},
+    io::Read,
+    os::unix::fs::MetadataExt,
+};
+
+use cpu::{FlagsRegister, MemoryBus, Registers, CPU};
+
+use crate::cpu::CartridgeHeader;
+
 fn main() {
-    let a: u8 = 200;
-    let b: u8 = 100;
+    let filename: String = String::from("/home/bleszerd/repositories/rubyagb/roms/blue.gb");
 
-    let result: u8 = a.wrapping_add(b);
+    let mut file = File::open(&filename).expect("No file found");
+    let metadata: fs::Metadata = fs::metadata(&filename).expect("Unable to read metada");
+    let mut buffer: Vec<u8> = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("Buffer overflow");
 
-    println!("{}", result);
+    let cartridge_header: CartridgeHeader = CartridgeHeader::init(buffer, metadata);
+
+    println!("{:?}", cartridge_header);
 }
